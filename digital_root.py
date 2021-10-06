baa49a9aed95bca2d801d4bc6d84752afe702b1c
basicosmic <1297820x@gmail.com>
2021-10-05 22:40:33 -0700

    fixed n>=10 in digital_root.py

--- digital_root.py
+++ digital_root.py
@@ -1,19 +1,13 @@
 def digital_root(n):
     def add_digit(n):
-        
         total = 0
         length = len(str(abs(n)))
         for i in range(length):
-            
             total += n%10
             n = n//10
         return total
-
-
-    while n > 10:
+    while n >= 10:
         n = add_digit(n)
-
     return n
-
 n = int(input())
 print(digital_root(n))
