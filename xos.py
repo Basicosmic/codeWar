62ba5216677792930f0f9fe473ecccbd498d4e32
Basicosmic <85536740+Basicosmic@users.noreply.github.com>
2021-10-09 01:01:47 -0700

    Update xos.py

--- xos.py
+++ xos.py
@@ -1,20 +1,6 @@
+#improved xo
 def xo(s):
-	counter_x = 0
-	counter_o = 0
-	length = len(str(s))
-	for i in range(length):
-		if 'x' == s[i].lower():
-			counter_x += 1
-		elif 'o' == s[i].lower():
-			counter_o += 1
-	if counter_x == counter_o:
-		return True
-	else:
-		return False 
-
-print('The number of x and o is equal')
+	s=s.lower()
+	return s.count('x')==s.count('o')
+print(xo('xxxoo'))
 print(xo('xxoo'))
-print(xo('xoxoo'))
-print(xo('xXoo'))
-print(xo('zpzp'))
-print(xo('xoaaaaa'))
